1252917840aba314729a9ba915f4af4a28aeb614
claudio.vilas <caitocd@gmail.com>
2023-10-10 13:31:56 -0300

    initial commit

---
+++ src/main/java/com/blog/BlogDesarolloApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDesarolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDesarolloApplication.class, args);
+	}
+
+}
